d8d2b6b871a80d7c0d3abe01f43a086e119a40a6
bnday01 <bnday01@msn.com>
2018-10-12 19:12:04 -0400

    Excersizes update

---
+++ Javascript/Excersizes/tests.js
@@ -0,0 +1,38 @@
+// EXCERSIZE 1, 2
+// 	function sayHello(){
+// 	console.log("Hellooo!");
+// 	}
+// sayHello();
+
+// EXCERSIZE 3
+// var sayBye = function() {
+// 	console.log("Bye");
+// }
+// sayBye();
+
+
+// EXCERSIZE 4
+// function sing(){
+// 	console.log("AAAAAHHHH");
+// 	console.log("TEEEEEEEE");
+// }
+// sing();
+
+// EXCERSIZE 5
+// function checkDriverAge(){
+
+// 	var age = prompt("What is your age?");
+
+// 	if (Number(age) < 18){
+// 		alert("Sorry, you are not old enough to drive this car. Powering off");
+// 	}
+// 	else if (Number(age)  > 18){
+// 		alert("Powering on! Enjoy the ride!");
+// 	}
+// 	else if (Number(age) === 18){
+// 		alert("Congratulations on your first year driving!");
+// 	}
+// }
+// checkDriverAge();
+
+
---
+++ Javascript/script.js
@@ -0,0 +1,94 @@
+
+// var database = [
+// {
+// 	username: "bnday01",
+// 	password: "getout2"
+// },
+// {
+// 	username: "googloo",
+// 	password: "google"
+// },
+// {
+// 	username: "msndotcom",
+// 	password: "microsoft"
+// }
+// ];
+
+// var newsfeed = [
+// {
+// 	username: "nicki124",
+// 	timeline: "cat videos"
+// },
+// {
+// 	username: "camedon16",
+// 	timeline: "stupidity"
+// },
+// {
+// 	username: "nextGenVideos",
+// 	timeline: "disrespectful girlfriends"
+// }
+// ];
+
+// var usernamePrompt = prompt("Enter Username");
+// var passwordPrompt = prompt("Enter Password");
+
+// function isUserValid(user,pass){
+// 	for (var i = 0; i < database.length; i++){
+// 		if (database[i].username === user && database[i].password === pass){
+// 			return true;
+// 		}
+// 	}
+// 	return false;
+
+// }
+
+// function signIn(){
+
+// 	if (isUserValid(usernamePrompt,passwordPrompt)){
+// 		return console.log(newsfeed);
+// 	}
+// 	else {
+// 		alert("Sorry, Username or Password incorrect");
+// 	}
+
+
+// }
+
+// signIn();
+
+var button = document.getElementsByTagName("button")[0];
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+
+function inputLength(){
+	return input.value.length;
+}
+
+function addItem(text){
+	var li = document.createElement("li");
+		li.appendChild(document.createTextNode(text));
+		ul.appendChild(li);
+		input.value ="";
+}
+
+function addClickListen(){
+	if ( inputLength() > 0){
+		addItem(input.value);
+	}
+	else { alert("Please Enter Text!");
+	}
+}
+
+function addKeyListen(event){
+	if (inputLength() === 0 && event.keyCode === 13){
+		alert("Please Enter Text!");
+	}
+	else if (inputLength() > 0 && event.keyCode === 13){
+		addItem(input.value);
+	}
+
+}
+
+button.addEventListener("click", addClickListen);
+
+input.addEventListener("keypress", addKeyListen);
